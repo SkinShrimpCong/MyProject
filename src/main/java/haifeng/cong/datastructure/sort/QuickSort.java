4d9c1c28e8411bfb0f77be508aebef6b41545686
conghaifeng <conghaifeng@ruijie.com.cn>
2020-06-08 16:39:49 +0800

    快速排序

--- src/main/java/haifeng/cong/datastructure/sort/QuickSort.java
+++ src/main/java/haifeng/cong/datastructure/sort/QuickSort.java
@@ -1,16 +1,42 @@
 package haifeng.cong.datastructure.sort;
 
+import java.util.Arrays;
+
 public class QuickSort {
 
     public static void main(String[] args) {
-
-    }
-
-    private static void singleSideQuickSort(int[] array) {
+        int[] array = {50,34,1000,4,-65,90};
         singleSideQuickSort(array, 0, array.length - 1);
+        System.out.print(Arrays.toString(array));
     }
 
     private static void singleSideQuickSort(int[] array, int startIndex, int endIndex) {
 
+        if (startIndex > endIndex) {
+            return;
+        }
+
+        int pivotIndex = partition(array, startIndex, endIndex);
+        singleSideQuickSort(array, startIndex, pivotIndex-1);
+        singleSideQuickSort(array, pivotIndex+1, endIndex);
+    }
+
+    private static int partition(int[] array, int startIndex, int endIndex) {
+        int pivot = array[startIndex];
+        int mark = startIndex;
+        for(int i = startIndex; i <= endIndex; ++i) {
+            if (array[i] < pivot) {
+                ++ mark;
+                swap(array, i, mark);
+            }
+        }
+        swap(array, startIndex, mark);
+        return mark;
+    }
+
+    private static void swap(int[] array, int firstIndex, int secondIndex) {
+        int temp = array[firstIndex];
+        array[firstIndex] = array[secondIndex];
+        array[secondIndex] = temp;
     }
 }
